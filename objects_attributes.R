7bc7733e8b69835228f2e9cd20b36cccf099ab10
xelita <admin@xelita.com>
2015-12-13 10:48:25 +0100

    objects and attributes

---
+++ objects_attributes.R
@@ -0,0 +1,19 @@
+print("numeric --------------------------------")
+x <- 12 + 5
+print(x)
+print(class(x))
+
+print("integer --------------------------------")
+x <- 12L + 5L
+print(x)
+print(class(x))
+
+print("logical --------------------------------")
+x <- TRUE
+print(x)
+print(class(x))
+
+print("character ------------------------------")
+x <- "hello world!"
+print(x)
+print(class(x))
